a9664d66800d2798d2402fdc6f5977beef529e7d
ethan-phillips26 <ethanphillips2005@gmail.com>
2025-04-20 19:24:25 -0500

    ndsu.edu email address is now need to sign in

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,15 +1,18 @@
 import { inject, Injectable } from '@angular/core';
-import { GoogleAuthProvider, signInAnonymously, signInWithPopup, signOut } from 'firebase/auth';
+import {
+  GoogleAuthProvider,
+  signInAnonymously,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
 import { Auth, onAuthStateChanged, User } from '@angular/fire/auth';
 import { BehaviorSubject } from 'rxjs';
 import { signal } from '@angular/core';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private auth = inject(Auth);
   private isLoggedIn = signal(false);
 
@@ -32,8 +35,27 @@ export class AuthService {
   }
   signIn() {
     const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ hd: 'ndsu.edu' });
+  
     return signInWithPopup(this.auth, provider)
+      .then(async (result) => {
+        const email = result.user.email || '';
+  
+        if (!email.endsWith('@ndsu.edu')) {
+          alert('You must use your NDSU email to sign in.');
+          await signOut(this.auth);
+          return;
+        }
+  
+        this.userSubject.next(result.user);
+        this.isLoggedIn.set(true);
+      })
+      .catch((error) => {
+        console.error('Google sign-in error:', error);
+        throw error;
+      });
   }
+  
 
   signOut() {
     const provider = new GoogleAuthProvider();
