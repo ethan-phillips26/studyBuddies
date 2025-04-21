262eeaf4ba516514a5010a864577c89fe774b57b
ethan-phillips26 <ethanphillips2005@gmail.com>
2025-04-20 19:15:20 -0500

    Added Stream tutorial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideRouter } from '@angular/router';
-import { provideFirestore, getFirestore} from '@angular/fire/firestore'
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
-
+import { TranslateModule } from '@ngx-translate/core';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMyVeEsJM4tw7DitWOWLAd1ACAyPpXXCg",
-  authDomain: "studybuddies-1150c.firebaseapp.com",
-  projectId: "studybuddies-1150c",
-  storageBucket: "studybuddies-1150c.firebasestorage.app",
-  messagingSenderId: "159344652152",
-  appId: "1:159344652152:web:068c56d52db7fa3d60628d"
+  apiKey: 'AIzaSyBMyVeEsJM4tw7DitWOWLAd1ACAyPpXXCg',
+  authDomain: 'studybuddies-1150c.firebaseapp.com',
+  projectId: 'studybuddies-1150c',
+  storageBucket: 'studybuddies-1150c.firebasestorage.app',
+  messagingSenderId: '159344652152',
+  appId: '1:159344652152:web:068c56d52db7fa3d60628d',
 };
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,6 +21,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth())
+    provideAuth(() => getAuth()),
+    importProvidersFrom(TranslateModule.forRoot())
   ],
 };
--- src/app/messenger-page/messenger-page.component.ts
+++ src/app/messenger-page/messenger-page.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { User } from 'stream-chat';
+import {
+  ChatClientService,
+  ChannelService,
+  StreamI18nService,
+  StreamAutocompleteTextareaModule,
+  StreamChatModule,
+} from 'stream-chat-angular';
 
 @Component({
   selector: 'app-messenger-page',
-  imports: [],
+  standalone: true,
+  imports: [TranslateModule, StreamAutocompleteTextareaModule, StreamChatModule],
   templateUrl: './messenger-page.component.html',
-  styleUrl: './messenger-page.component.css'
+  styleUrls: ['./messenger-page.component.css'],
 })
-export class MessengerPageComponent {
+export class MessengerPageComponent implements OnInit {
+  constructor(
+    private chatService: ChatClientService,
+    private channelService: ChannelService,
+    private streamI18nService: StreamI18nService,
+  ) {
+    const apiKey = 'dz5f4d5kzrue';
+    const userId = 'aged-fog-3';
+    const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWdlZC1mb2ctMyIsImV4cCI6MTc0NTIwNTY1NH0.eS0xqzIdNATf-69nT-YMfrt4SwPFvDevcyfQDNzVLY4';
+    const userName = 'aged';
 
+    const user: User = {
+      id: userId,
+      name: userName,
+      image: `https://getstream.io/random_png/?name=${userName}`,
+    };
+
+    this.chatService.init(apiKey, user, userToken);
+    this.streamI18nService.setTranslation();
+  }
+
+  async ngOnInit() {
+    const channel = this.chatService.chatClient.channel('messaging', 'talking-about-angular', {
+      // add as many custom fields as you'd like
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
+      name: 'Talking about Angular',
+    });
+    await channel.create();
+    this.channelService.init({
+      type: 'messaging',
+      id: { $eq: 'talking-about-angular' },
+    });
+  }
 }
