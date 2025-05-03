410d6d0698f399855fd29ee705729ea816462786
Kimberly Olson <kimberly45olson@gmail.com>
2025-05-03 18:52:19 -0500

    initial html for profile page

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './profile-page.component.css'
 })
 export class ProfilePageComponent {
+  imagePreview: string | ArrayBuffer | null = null;
 
+  onFileSelected(event: Event): void {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file && file.type.startsWith('image/')) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      };
+      reader.readAsDataURL(file);
+    } else {
+      this.imagePreview = null;
+    }
+  }
+
+  isEditing = false; // Tracks whether editing mode is active
+
+  toggleEdit() {
+    this.isEditing = !this.isEditing; // Toggle edit mode on button click
+  }
 }
